fee3f629f02dcac93a91a8493ec4c282ea08fe95
StephOkoli <153391848+StephOkoli@users.noreply.github.com>
2026-04-17 17:19:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const form = document.querySelector("form");
 const status = document.getElementById("status");
 
-form.addEventListener("submit", function() {
-    status.innerText = "Procressing request..."
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    status.innerText = "Transmitting...";
+
+    setTimeout(() => {
+        status.innerText = "Verifying...";
+    }, 1000);
+
+    const data = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: { Accept: "application/json" }
+    });
+
+    if (response.ok) {
+        setTimeout(() => {
+            status.innerText = "Access Granted";
+        }, 2000);
+
+    form.reset();
+    } else {
+        status.innerText = "Transmission Failed"
+    }
 })
